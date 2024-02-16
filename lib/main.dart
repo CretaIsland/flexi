22e115a21b9e41f141bb80168b5d2ee145c8b3e3
skpark33 <skpark@sqisoft.com>
2024-02-16 18:28:06 +0900

    20240216-02-skpark

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:flexi/data_io/host_manager.dart';
+// ignore: depend_on_referenced_packages
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+
+import 'model/host_model.dart';
 
 void main() {
+  Logger.root.level = Level.INFO;
   runApp(const FlexiApp());
 }
 
@@ -37,6 +44,8 @@ class FlexiApp extends StatelessWidget {
 }
 
 class FlexiHomePage extends StatefulWidget {
+  static HostManager hostManagerHolder = HostManager();
+
   const FlexiHomePage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -59,13 +68,23 @@ class _FlexiHomePageState extends State<FlexiHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
+      FlexiHomePage.hostManagerHolder.autoDiscovery.stopBroadcast();
+      FlexiHomePage.hostManagerHolder.autoDiscovery.startBroadcast();
+
+      // This call to se
+      //tState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    FlexiHomePage.hostManagerHolder.autoDiscovery.stopBroadcast();
   }
 
   @override
@@ -76,50 +95,65 @@ class _FlexiHomePageState extends State<FlexiHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the FlexiHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<HostManager>.value(
+          value: FlexiHomePage.hostManagerHolder,
         ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the FlexiHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Consumer<HostManager>(builder: (context, hostManager, child) {
+          return Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              //
+              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+              // action in the IDE, or press "p" in the console), to see the
+              // wireframe for each widget.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                ListView(
+                  children: hostManager.modelList.map((ele) {
+                    HostModel hostModel = ele as HostModel;
+                    return Text('${hostModel.hostName}, ${hostModel.ip}, ${hostModel.isConnected}');
+                  }).toList(),
+                )
+              ],
+            ),
+          );
+        }),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
