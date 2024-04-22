68ef6afb98332f528cb0bb814fb5a5bc088816e4
Hnarae <nr.han@sqisoft.com>
2024-04-22 11:23:10 +0900

    20240422-01-nr.han 디바이스 정보 화면 UI

--- lib/view/device/device_info_screen.dart
+++ lib/view/device/device_info_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flexi/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -15,8 +16,12 @@ class DeviceInfoScreen extends ConsumerStatefulWidget {
   ConsumerState<DeviceInfoScreen> createState() => _DeviceInfoScreenState();
 }
 
+final volumeProvider = StateProvider<double>((ref) => 0);
+
 
 class _DeviceInfoScreenState extends ConsumerState<DeviceInfoScreen> {
+
+
     
   @override
   Widget build(BuildContext context) {
@@ -41,10 +46,160 @@ class _DeviceInfoScreenState extends ConsumerState<DeviceInfoScreen> {
                 )
               ],
             ),
+            SizedBox(height: screenHeight * .03),
+            Text("Content", style: FlexiFont.regular14),
+            SizedBox(height: screenHeight * .01),
+            Container(
+              width: screenWidth * .89,
+              height: screenHeight * .0675,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(screenHeight * .01)
+              ),
+              child: Center(
+                child: Container(
+                  width: screenWidth * .82,
+                  height: screenHeight * .0375,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(screenHeight * .005)
+                  ),
+                )
+              ),
+            ),
+            SizedBox(height: screenHeight * .015),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Network", style: FlexiFont.regular14,),
+                    SizedBox(height: screenHeight * .01),
+                    Container(
+                      width: screenWidth * .43,
+                      height: screenHeight * .125,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(screenHeight * .01)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.wifi_rounded, size: screenHeight * .045, color: FlexiColor.primary),
+                          Text("SSID", style: FlexiFont.semiBold14.copyWith(color: FlexiColor.primary))
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Bluetooth", style: FlexiFont.regular14,),
+                    SizedBox(height: screenHeight * .01),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: screenWidth * .43,
+                        height: screenHeight * .125,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(screenHeight * .01)
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.bluetooth_rounded, size: screenHeight * .045, color: FlexiColor.primary),
+                            Text("device name", style: FlexiFont.semiBold14.copyWith(color: FlexiColor.primary))
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: screenHeight * .015),
+            Text("Device Name", style: FlexiFont.regular14),
+            SizedBox(height: screenHeight * .015),
+            deviceInfoTextField(),
+            SizedBox(height: screenHeight * .015),
+            Text("Device Volume", style: FlexiFont.regular14),
+            SizedBox(height: screenHeight * .015),
+            Container(
+              width: screenWidth * .89,
+              height: screenHeight * .06,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(screenHeight * .01)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () => ref.watch(volumeProvider.notifier).state = 0,
+                    icon: Icon(Icons.volume_mute_rounded, color: FlexiColor.primary),
+                    iconSize: screenHeight * .03,
+                  ),
+                  SizedBox(
+                    width: screenWidth * .6,
+                    height: screenHeight * .005,
+                    child: Slider(
+                      value: ref.watch(volumeProvider), 
+                      min: 0,
+                      max: 100,
+                      thumbColor: FlexiColor.primary,
+                      activeColor: FlexiColor.primary,
+                      onChanged: (value) {
+                        ref.watch(volumeProvider.notifier).state = value;
+                      }
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () => ref.watch(volumeProvider.notifier).state = 100, 
+                    icon: Icon(Icons.volume_up_rounded, color: FlexiColor.primary),
+                    iconSize: screenHeight * .03,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: screenHeight * .015),
+            Text("Devie Timezone", style: FlexiFont.regular14),
+            SizedBox(height: screenHeight * .015),
+            deviceInfoTextField(),
+            SizedBox(height: screenHeight * .015),
+            Text("Network", style: FlexiFont.regular14),
+            SizedBox(height: screenHeight * .015),
+            deviceInfoTextField()
           ],
         ),
       ),
       bottomNavigationBar: const FlexiBottomNaviagtionBar(),
+    );
+  }
+
+  Widget deviceInfoTextField() {
+    return SizedBox(
+      width: screenWidth * .89,
+      height: screenHeight * .06,
+      child: TextField(
+        controller: TextEditingController(),
+        style: FlexiFont.regular16,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(screenHeight * .01),
+            borderSide: BorderSide(color: FlexiColor.grey[400]!)
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(screenHeight * .01),
+            borderSide: BorderSide(color: FlexiColor.grey[400]!)
+          ),
+          border: InputBorder.none,
+          fillColor: Colors.white,
+          filled: true
+        ),
+      ),
     );
   }
 
