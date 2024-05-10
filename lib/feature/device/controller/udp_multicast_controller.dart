3c5b813641b079daabb69071194130af669807f5
Hnarae <nr.han@sqisoft.com>
2024-05-10 16:44:46 +0900

    20240510-02-nr.han udp multicast controller fix

--- lib/feature/device/controller/udp_multicast_controller.dart
+++ lib/feature/device/controller/udp_multicast_controller.dart
@@ -10,58 +10,39 @@ part 'udp_multicast_controller.g.dart';
 
 
 @riverpod
-Stream<String> uDPMulticastStream(UDPMulticastStreamRef ref) {
-  final uDPMulticastController = ref.read(uDPMulticastControllerProvider.notifier);
-  return uDPMulticastController.udpMulticastStream();
-}
-
-@riverpod
-List<String> uDPMulticastDatas(UDPMulticastDatasRef ref) {
-  final uDPMulticastController = ref.read(uDPMulticastControllerProvider.notifier);
-  return uDPMulticastController.dataList();
-}
-
-
-@riverpod
 class UDPMulticastController extends _$UDPMulticastController {
 
   late RawDatagramSocket _socket;
-  late StreamController<String> _streamController;
-  late List<String> _dataList;
-
-  Stream<String> udpMulticastStream() => _streamController.stream;
-  List<String> dataList() => _dataList;
+  late StreamController<List<String>> _controller;
 
   @override
-  void build() {
+  Stream<List<String>> build() {
     ref.onDispose(() {
-      print("<<<<<<< UDPMulticastController dispose <<<<<<<");
       _socket.leaveMulticast(Config.udpMulticastAddress);
       _socket.close();
-    });
-    print(">>>>>>> UDPMulticastController build >>>>>>");
 
-    _streamController = StreamController<String>();
-    _dataList = [];
-    initialize();
+      _controller.close();
+    });
+    _controller = StreamController<List<String>>();
+
+    _init();
+
+    return _controller.stream;
   }
 
-  Future<void> initialize() async {
-    _socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, Config.udpMulticastPort);
+  Future<void> _init() async {
+    _socket = await RawDatagramSocket.bind(
+        InternetAddress.anyIPv4, Config.udpMulticastPort);
+
     _socket.joinMulticast(Config.udpMulticastAddress);
 
     _socket.listen((event) {
       Datagram? d = _socket.receive();
-      if(d == null) return;
+      if (d == null) return;
       String message = String.fromCharCodes(d.data).trim();
-      _streamController.add(message);
-      _dataList.add(message);
+      _controller.add([message]);
     });
   }
 
-  void sendData(String data) {
-    _socket.send(
-        data.codeUnits, Config.udpMulticastAddress, Config.udpMulticastPort);
-  }
 
 }
--- lib/feature/device/controller/udp_multicast_controller.g.dart
+++ lib/feature/device/controller/udp_multicast_controller.g.dart
@@ -6,44 +6,13 @@ part of 'udp_multicast_controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$uDPMulticastStreamHash() =>
-    r'f015972f78780fac375103fe4060982591d2472a';
-
-/// See also [uDPMulticastStream].
-@ProviderFor(uDPMulticastStream)
-final uDPMulticastStreamProvider = AutoDisposeStreamProvider<String>.internal(
-  uDPMulticastStream,
-  name: r'uDPMulticastStreamProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$uDPMulticastStreamHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef UDPMulticastStreamRef = AutoDisposeStreamProviderRef<String>;
-String _$uDPMulticastDatasHash() => r'1e12ad0e35252fad4dc6f469c829cba53a7caae2';
-
-/// See also [uDPMulticastDatas].
-@ProviderFor(uDPMulticastDatas)
-final uDPMulticastDatasProvider = AutoDisposeProvider<List<String>>.internal(
-  uDPMulticastDatas,
-  name: r'uDPMulticastDatasProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$uDPMulticastDatasHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef UDPMulticastDatasRef = AutoDisposeProviderRef<List<String>>;
 String _$uDPMulticastControllerHash() =>
-    r'5f59a20af05cc50925e211f3f5efb33006677f28';
+    r'cf2633220c124632917c70666350b2bb200b4e25';
 
 /// See also [UDPMulticastController].
 @ProviderFor(UDPMulticastController)
-final uDPMulticastControllerProvider =
-    AutoDisposeNotifierProvider<UDPMulticastController, void>.internal(
+final uDPMulticastControllerProvider = AutoDisposeStreamNotifierProvider<
+    UDPMulticastController, List<String>>.internal(
   UDPMulticastController.new,
   name: r'uDPMulticastControllerProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -53,6 +22,6 @@ final uDPMulticastControllerProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$UDPMulticastController = AutoDisposeNotifier<void>;
+typedef _$UDPMulticastController = AutoDisposeStreamNotifier<List<String>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
